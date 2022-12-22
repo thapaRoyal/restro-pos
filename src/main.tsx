c9cd59567dfd2713c063da6b6129b393f448fc0a
thapaRoyal <thaparoyal17@gmail.com>
2022-12-22 20:22:41 +0545

    react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hi</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div className="bg-gray-900">
-      <App />
-    </div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
